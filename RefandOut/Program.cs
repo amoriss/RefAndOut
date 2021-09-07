c4892d81156f3fb44272329d92ff516de55f2bcf
Amoriss <amoriss@protonmail.com>
2021-09-07 13:36:13 -0500

    initial commit

--- RefandOut/Program.cs
+++ RefandOut/Program.cs
@@ -9,22 +9,48 @@ namespace RefandOut
         //TODO Call the method in the main and make sure the method changes the number of a initialized variable and returns a bool.
         public static bool SubtractHealth(ref int x)
         {
-      
+            x--;
+            if (x > 0)
+            {
+                return true; 
+            }
+            return false; 
+
         }
 
         //TODO create method that uses the out keyword. Call the method in the main.
         
 
-        public int Sum(int x, int y, out  bool even)
+        public static int Sum(int x, int y, out  bool even)
         {
-            //use this metho or make your own.
+            //use this method or make your own.
+            int sum = x + y;
+            if (sum%2==0)
+            {
+                even = true;
+            }
+            else
+            {
+                even = false; 
+            }
+
+            return sum; 
+            //bool even = int.TryParse( , out sum);
 
         }
 
 
         static void Main(string[] args)
         {
+            //ref
+            int x = 17;
+            var result = (SubtractHealth(ref x));
+            Console.WriteLine(x);
+            Console.WriteLine(result);
 
+            //out
+            bool isEven; 
+            var sum = Sum(1236, 1232, out isEven);
     
         }
 
